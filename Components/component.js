3983e33d70064f94c60735b7d75495e6693d40fd
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-02-10 00:12:23 +0530

    feat: dynamic chip creating function added

--- Components/component.js
+++ Components/component.js
@@ -7,3 +7,28 @@ const hideBadge = () => {
 }
 
 toggleBtn.addEventListener("click", hideBadge);
+
+
+// dynamically add chip
+const chipContainer = document.querySelector("#chip_container");
+const chipInput = document.querySelector("#chip_input");
+
+const deleteChip = (event)=>{
+  event.target.parentElement.remove();
+}
+
+const addChip = (event)=>{
+  if(event.code == "Enter"){
+    let span = document.createElement("span");
+    let icon = document.createElement("i");
+    icon.setAttribute("class", "chip-cross fas fa-times-circle");
+    icon.addEventListener("click", deleteChip);
+    span.innerText = event.target.value;
+    span.setAttribute("class", "chip");
+    span.append(icon);
+    chipContainer.prepend(span);
+    event.target.value = "";
+  }
+}
+
+chipInput.addEventListener("keydown", addChip);
